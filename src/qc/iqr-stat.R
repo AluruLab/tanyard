022e0922f92910022b9f1088c01e0e7ef0131237
Sriram P C <sriram.pc@gmail.com>
2018-08-14 16:56:59 -0400

    use of ggplot2 for density plot

--- src/qc/iqr-stat.R
+++ src/qc/iqr-stat.R
@@ -1,4 +1,5 @@
-library(lattice)
+#library(lattice)
+library(ggplot2)
 
 rle_densityplot <- function(Wd, outfile, sfx){
     Dirs=dir(Wd, full.names=TRUE)
@@ -10,9 +11,15 @@ rle_densityplot <- function(Wd, outfile, sfx){
       y=append(y,x[,3])
       if (max(abs(x[,3])) > 0.5) print(d)
     }
-    trellis.device(device="pdf", file=outfile)
-    densityplot(y, main=paste("Density of IQR of ", sfx, sep=" "),
-                   xlab = paste(sfx, "values"))
+    # trellis.device(device="pdf", file=outfile)
+    # densityplot(y, main=paste("Density of IQR of ", sfx, sep=" "),
+    #               xlab = paste(sfx, "values"))
+    ysx = summary(y)
+    df = data.frame(median = y)
+    px = ggplot(df) + geom_density(aes(x = median)) +
+         geom_rug(aes(x = median, y = 0), position = position_jitter(height = 0)) +
+         xlab(paste(sfx, "; mean = ", as.character(mean(y)), "; sd = ", as.character(sd(y)), sep=""))
+    ggsave(filename=outfile, plot=px)
 }
 
 args = commandArgs(trailingOnly=TRUE)
