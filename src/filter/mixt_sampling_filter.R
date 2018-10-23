a589a646a26305527415d7ed9da9eb34c2eed2fc
Sriram P C <sriram.pc@gmail.com>
2018-10-23 13:46:22 -0400

    sampling of mixt adaptive filtering

--- src/filter/mixt_sampling_filter.R
+++ src/filter/mixt_sampling_filter.R
@@ -15,7 +15,7 @@ select_genes = function(eqdat, nk=3){
     Midx
 }
 
-mixt_filter = function(infile, outfile, 
+mixt_filter_subset = function(infile, outfile, 
                        start = 500, slab = 500){
     M = read.table(infile, head=T, row.names=1)
     #print()
@@ -31,9 +31,38 @@ mixt_filter = function(infile, outfile,
                 sep="\t", quote=F)
 }
 
+mixt_filter_sample = function(infile, outfile, 
+                              nsample=20,
+                              start = 500, slab = 500){
+    M = read.table(infile, head=T, row.names=1)
+    #print()
+    nsizes = rev(seq(start, dim(M)[2], by=slab))
+    #ngenes = rep(0, length(nsizes))
+    ngenes = matrix(0, nrow = nsizes, ncol = nsample)
+    for(x in (1:length(nsizes))){
+        for(y in (1:nsample)){
+            sx = sample(1:dim(M)[2], nsizes[x])
+            Midx = select_genes(M[, sx])
+            ngenes[x, y] = length(Midx)
+        }
+        print(c(nsizes[x], summary(ngenes[x,])))
+        M =  M[, 1:nsizes[x]]
+    }
+    mdf = data.frame(
+        cbind(ngenes, t(apply(ngenes, 1 ,summary)))
+    )
+    mdf$data = nsizes
+    write.table(file=outfile, mdf, sep="\t", quote=F)
+}
+
 args = commandArgs(trailingOnly=TRUE)
 if(length(args) == 2){
-    mixt_filter(infile=args[1], outfile=args[2])
+    mixt_filter_subset(infile=args[1],
+                       outfile=args[2])
+} else if(length(args) == 3){
+    mixt_filter_sample(infile=args[1],
+                       outfile=args[2], 
+                       nsample=as.numeric(args[3]))
 } else {
-   print("Usage: Rscript mixt_filter.R <INFILE> <OUTFILE> ")
+   print("Usage: Rscript mixt_sampling_filter.R <INFILE> <OUTFILE> [<NSAMPLES>]")
 }
