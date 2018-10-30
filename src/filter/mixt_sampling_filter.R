f58805da18459efe192d85bfdcaf64b9e79fff4a
Sriram P C <sriram.pc@gmail.com>
2018-10-30 09:45:30 -0400

    adding parallel generation of filter

--- src/filter/mixt_sampling_filter.R
+++ src/filter/mixt_sampling_filter.R
@@ -1,21 +1,28 @@
 library(mixtools)
+library(doParallel)
+dp.ncores = 25
+mixt_filter_sample = function(infile, outfile, 
+                              nsample=20,
+                              start = 1000,
+                              slab = 200,
+                              ntop = 10){
 
-l2variance = function(eqdat){
+ l2variance = function(eqdat){
     gvar = apply(eqdat, 1, var)
     gsm = data.frame("gene" = names(gvar), "var" = gvar)
     gsm$var = log2(gsm$var)  
     gsm
-}
+ }
 
-select_genes = function(eqdat, nk=3){
+ select_genes = function(eqdat, nk=3){
     gsm = l2variance(eqdat)
-    emrk = normalmixEM(gsm$var, k=nk)
+    emrk = normalmixEM(gsm$var, k=nk, maxit=2500)
     rjmu = which.min(emrk$mu)
     Midx = which(max.col(emrk$posterior) != rjmu)
     Midx
-}
+ }
 
-mixt_filter_subset = function(infile, outfile, 
+ mixt_filter_subset = function(infile, outfile, 
                        start = 500, slab = 500){
     M = read.table(infile, head=T, row.names=1)
     #print()
@@ -29,43 +36,51 @@ mixt_filter_subset = function(infile, outfile,
     }
     write.table(file=outfile, data.frame(data = nsizes, genes = ngenes),
                 sep="\t", quote=F)
-}
+ }
 
-mixt_filter_sample = function(infile, outfile, 
-                              nsample=20,
-                              start = 1000,
-                              slab = 200,
-                              ntop = 5){
+ data_dim = function(infile){
+    M = read.table(infile, head=T, row.names=1)
+    dim(M)
+ }
+
+
+  mixt_sample = function(M, nsz, ntop, nsample, outdir) {
+   dM = dim(M)
+   topx = matrix(0, nrow = ntop + 1, ncol = dM[1] + 1)
+   ngenes = rep(0, nsample)
+   for(y in (1:nsample)) {
+        sx = sample(1:dM[2], nsz)
+        Midx = select_genes(M[, sx])
+        ngenes[y] = length(Midx)
+        # update best ntop
+        topx[ntop + 1, 1:(1 + length(Midx))] = c(length(Midx), Midx)
+        rsel = (rank(t(topx[,1]), ties.method="last") > 1)
+        topx[1:ntop, ] = topx[rsel, ]
+        topx[ntop+1, ] = 0
+   }
+   print(c(nsz, summary(ngenes)))
+   toutf = paste(outdir,
+                 paste("mixt-top-", ntop, "-sample-", nsz,  ".csv", sep=""),
+                sep="/")
+   write.table(file=toutf, topx, sep="\t")
+   names(ngenes) = paste("S", 1:length(ngenes))
+   c(data = nsz, ngenes, summary(ngenes))
+ }
     outdir = dirname(outfile)
     M = read.table(infile, head=T, row.names=1)
-    #print()
-    nsizes = rev(seq(start, dim(M)[2], by=slab))
-    #ngenes = rep(0, length(nsizes))
-    topx = matrix(0, nrow = ntop + 1, ncol = dim(M)[1] + 1)
+    dM = dim(M)
+    nsizes = rev(seq(start, dM[2], by=slab))
     ngenes = matrix(0, nrow = length(nsizes), ncol = nsample)
-    for(x in (1:length(nsizes))){
-        for(y in (1:nsample)){
-            sx = sample(1:dim(M)[2], nsizes[x])
-            Midx = select_genes(M[, sx])
-            ngenes[x, y] = length(Midx)
-            # update best ntop
-            topx[ntop + 1, 1:(1 + length(Midx))] = c(length(Midx), Midx)
-            rsel = rank(topx[,1], ties.method="last") < (1 + ntop)
-            topx[1:ntop, ] = topx[rsel, ]
-            topx[ntop+1, ] = 0
-        }
-        print(c(nsizes[x], summary(ngenes[x,])))
-        M =  M[, 1:nsizes[x]]
+    # for(x in (1:length(nsizes)))
+    cl = makeCluster(dp.ncores)
+    registerDoParallel(cl) 
+    mdf = foreach(nsz = nsizes, .combine=cbind, .packages='mixtools') %dopar% {
+        mixt_sample(M, nsz, ntop, nsample, outdir)
     }
-    mdf = data.frame(
-        cbind(ngenes, t(apply(ngenes, 1 ,summary)))
-    )
-    mdf$data = nsizes
-    write.table(file=outfile, mdf, sep="\t", quote=F)
-    toutf = paste(outdir,
-                  paste("mixt-top-", x, ".csv", sep=""),
-                  sep="/")
-    write.table(file=toutf, topx, sep="\t")
+    stopCluster(cl)
+    # mdf = data.frame(cbind(ngenes, t(apply(ngenes, 1 ,summary))))
+    # mdf$data = nsizes
+    write.table(file=outfile, data.frame(t(mdf)), sep="\t", quote=F)
 }
 
 args = commandArgs(trailingOnly=TRUE)
@@ -79,6 +94,12 @@ if(length(args) == 2){
                        outfile=ofile, 
                        nsample=as.numeric(args[3]))
 
+} else if(length(args) == 4){
+    ofile = paste(args[2], "mixt-samples.csv", sep="/")
+    mixt_filter_sample(infile=args[1],
+                       outfile=ofile, 
+                       nsample=as.numeric(args[3]),
+                       start=as.numeric(args[4]))
 } else if(length(args) == 5){
     ofile = paste(args[2], "mixt-samples.csv", sep="/")
     mixt_filter_sample(infile=args[1],
@@ -86,6 +107,14 @@ if(length(args) == 2){
                        nsample=as.numeric(args[3]),
                        start=as.numeric(args[4]),
                        slab=as.numeric(args[5]))
+} else if(length(args) == 6){
+    ofile = paste(args[2], "mixt-samples.csv", sep="/")
+    mixt_filter_sample(infile=args[1],
+                       outfile=ofile, 
+                       nsample=as.numeric(args[3]),
+                       start=as.numeric(args[4]),
+                       slab=as.numeric(args[5]),
+                       ntop=as.numeric(args[6]))
 } else {
-   print("Usage: Rscript mixt_sampling_filter.R <INFILE> <OUTDIR> [<NSAMPLES>] [<START> <SLAB>]")
+   print("Usage: Rscript mixt_sampling_filter.R <INFILE> <OUTDIR> [<NSAMPLES> <START> <SLAB> <NTOP>]")
 }
