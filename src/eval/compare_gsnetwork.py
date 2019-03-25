a55e3c40468656ac407f1feb2011dc96678c5c0e
Sriram P C <sriram.pc@gmail.com>
2019-03-25 15:09:03 -0400

    fix bugs

--- src/eval/compare_gsnetwork.py
+++ src/eval/compare_gsnetwork.py
@@ -51,6 +51,11 @@ def eval_network(annot_file, net_file, gs_file, max_dist):
     rv_net = load_reveng_network(net_file)
     gs_net = map_probes(load_gsnetwork(gs_file), annot_df)
     rv_net_graph = nx.from_pandas_edgelist(rv_net, edge_attr='wt')
+    gs_nedges = gs_net.shape[0]
+    gs_nodes = set(gs_net.TFPROBE) | set(gs_net.TARGETPROBE)
+    gs_common_nodes = sum((1 if x in rv_net_graph else 0 for x in gs_nodes))
+    gs_common_edges = sum((1 if (x in rv_net_graph and y in rv_net_graph) else 0 
+                           for x,y in zip(gs_net.TFPROBE, gs_net.TARGETPROBE)))
     gs_spath = [(nx.shortest_path_length(rv_net_graph, x, y), x, y) 
                     if x in rv_net_graph and y in rv_net_graph else (float("inf"), x, y) 
                     for x,y in zip(gs_net.TFPROBE, gs_net.TARGETPROBE) ]
@@ -59,8 +64,16 @@ def eval_network(annot_file, net_file, gs_file, max_dist):
     for x, _, _ in gs_spath:
         if x <= max_dist:
             dist_histogram[x] += 1
-    print(dist_histogram)
-    return dist_histogram
+    dist_histogram_df = pd.DataFrame(data={'DIST'   : [x for x in range(max_dist+1)],
+                                           'CNT.'   : dist_histogram,
+                                           'DPCTX'  : [float(x)*100/gs_nedges for x in dist_histogram],
+                                           'DPCTY'  : [float(x)*100/gs_common_edges for x in dist_histogram],
+                                           'GSNDS'  : [len(gs_nodes) for _ in range(max_dist+1)],
+                                           'GSEDG'  : [gs_nedges for _ in range(max_dist+1)],
+                                           'GSRVN'  : [gs_common_nodes for _ in range(max_dist+1)],
+                                           'GSRVE'  : [gs_common_edges for _ in range(max_dist+1)]})
+    print(dist_histogram_df)
+    return dist_histogram_df
 
 
 if __name__ == "__main__":
@@ -73,4 +86,5 @@ if __name__ == "__main__":
         help="gold standard network (tab seperated file of TF-TARGET interactions)")
     parser.add_argument("-d", "--dist", type=int, default=3,
                         help="max. number of hops allowed")
-    parser.parse_args()
+    args = parser.parse_args()
+    eval_network(args.annotation_file, args.reveng_network_file, args.gs_network_file, args.dist)
