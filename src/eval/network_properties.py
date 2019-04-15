30381d5481e121beaec86381299024f1a2d7377d
Sriram P C <sriram.pc@gmail.com>
2019-04-15 13:22:20 -0400

    fix networkx properites bug

--- src/eval/network_properties.py
+++ src/eval/network_properties.py
@@ -7,9 +7,9 @@ def main(network_file: str):
     net_df = load_reveng_network(network_file)
     rev_net = nx.from_pandas_edgelist(net_df, edge_attr='wt')
     network_props = """ Nodes {} Edges {} Density {} """
-    print(network_props % (rev_net.number_of_nodes,
-                           rev_net.number_of_edges,
-                           rev_net.density))
+    print(network_props % (nx.number_of_nodes(rev_net),
+                           nx.number_of_edges(rev_net),
+                           nx.density(rev_net)))
 
 
 if __name__ == "__main__":
