15802013ce9fd832c97167b9051a66530548cf8b
Sriram P C <sriram.pc@gmail.com>
2019-04-15 13:15:10 -0400

    add network properties script

---
+++ src/eval/network_properties.py
@@ -0,0 +1,21 @@
+import argparse
+import networkx as nx
+from data_utils import load_reveng_network
+
+
+def main(network_file: str):
+    net_df = load_reveng_network(network_file)
+    rev_net = nx.from_pandas_edgelist(net_df, edge_attr='wt')
+    network_props = """ Nodes {} Edges {} Density {} """
+    print(network_props % (rev_net.number_of_nodes,
+                           rev_net.number_of_edges,
+                           rev_net.density))
+
+
+if __name__ == "__main__":
+    PROG_DESC = """Describe network statistics"""
+    ARGPARSER = argparse.ArgumentParser(description=PROG_DESC)
+    ARGPARSER.add_argument("network_file")
+    CMDARGS = ARGPARSER.parse_args()
+    main(CMDARGS.network_file)
+    
