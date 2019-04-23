94cd1b67ef2e8c7ddc71e343d459a057ef9d00d5
Sriram P C <sriram.pc@gmail.com>
2019-04-23 11:26:48 -0400

    fix bug in data utils

--- src/eval/network_threshold_stats.py
+++ src/eval/network_threshold_stats.py
@@ -4,12 +4,18 @@ import pandas as pd
 
 
 def main(network_file, output_file):
-    net_df = pd.read_csv(network_file, sep="\t")
+    net_df = pd.read_csv(network_file, sep="\t", index_col=0)
     th_lst = [float(x)/100 for x in range(1, 100)]
-    edge_ct = [(sum(np.sum(abs(net_df) > x, axis=1)) - float(net_df.shape[0]))/2.0
-               for x in th_lst]
+    edge_ct = [0 for _ in range(1, 100)]
+    node_ct = [0 for _ in range(1, 100)]
+    for i,j in enumerate(range(1, 100)):
+        x = j/100 
+        edge_inc = np.sum(abs(net_df) > x, axis=1) - 1
+        edge_ct[i] = sum(edge_inc)/2.0
+        node_ct[i] = sum(edge_inc > 0)
     out_df = pd.DataFrame({"THRESHOLD": th_lst,
-                           "EDGE_COUNT": edge_ct})
+                           "EDGE_COUNT": edge_ct,
+                           "NODE_COUNT": node_ct})
     out_df.to_csv(output_file)
 
 
