4327cd90640fac9d28eb2e6e6815b4b3bc1814ca
Sriram P C <sriram.pc@gmail.com>
2019-09-30 12:48:35 -0400

    update compare scripts

--- src/eval/compare_gsnetwork.py
+++ src/eval/compare_gsnetwork.py
@@ -20,17 +20,21 @@ def shortest_path(net_graph, src, tgt):
     return spath
 
 def select_edges(net_df: pd.DataFrame, wt_attr_name: str = 'wt',
-                 max_edges: int = None):
+                 max_edges: int = None,
+                 reverse_order: bool = False):
     if max_edges is None or max_edges >= net_df.shape[0]:
         return net_df
     cur_cols = net_df.columns
     maxwt_attr_name = wt_attr_name + '_max'
     net_df[maxwt_attr_name] = net_df[wt_attr_name].abs()
-    net_df = net_df.nlargest(n=max_edges, columns=maxwt_attr_name)
+    if reverse_order is True:
+       net_df = net_df.nsmallest(n=max_edges, columns=maxwt_attr_name)
+    else:
+       net_df = net_df.nlargest(n=max_edges, columns=maxwt_attr_name)
     #print(net_df.iloc[0, :])
     return net_df.loc[:, cur_cols]
 
-def eval_network(rv_net, gs_net, max_dist, tf_col='TFPROBE', tgt_col='TARGETPROBE'):
+def eval_network(rv_net, gs_net, max_dist, wt_attr='wt', tf_col='TFPROBE', tgt_col='TARGETPROBE'):
     gs_nedges = gs_net.shape[0]
     gs_tf_nodes = set(gs_net.loc[:, tf_col])
     gs_tgt_nodes = set(gs_net.loc[:, tgt_col])
@@ -39,7 +43,7 @@ def eval_network(rv_net, gs_net, max_dist, tf_col='TFPROBE', tgt_col='TARGETPROB
     gs_fls_edges = list(gs_pos_edges - gs_tru_edges)
     gs_nodes = gs_tf_nodes | gs_tgt_nodes
     #rv_net_nodes = set(rv_net.source) | set(rv_net.target)
-    rv_net_graph = nx.from_pandas_edgelist(rv_net, edge_attr='wt')
+    rv_net_graph = nx.from_pandas_edgelist(rv_net, edge_attr=wt_attr)
     gs_common_nodes = sum((1 if x in rv_net_graph else 0 for x in gs_nodes))
     gs_common_edges = sum((1 if (x in rv_net_graph and y in rv_net_graph) else 0
                            for x, y in zip(gs_net.loc[:, tf_col], gs_net.loc[:, tgt_col])))
@@ -81,6 +85,8 @@ def eval_network(rv_net, gs_net, max_dist, tf_col='TFPROBE', tgt_col='TARGETPROB
                 pass
     prec = (float(tp_cts)/(tp_cts + fp_cts)) if (tp_cts + fp_cts) > 0 else 0
     hist_data = {
+        'NVRT'  : nx.number_of_nodes(rv_net_graph),
+        'NEDG'  : nx.number_of_edges(rv_net_graph),
         'DIST'  : [x for x in range(max_dist+1)],
         'EDGN'  : dist_histogram,
         'FPCTS' : [(fp_cts, tp_cts, prec, prec*100) for _ in range(max_dist+1)],
@@ -100,19 +106,28 @@ def eval_network(rv_net, gs_net, max_dist, tf_col='TFPROBE', tgt_col='TARGETPROB
     return hist_data
 
 
-def eval_network_probes(annot_file, net_file, gs_file, wt_attr, max_dist, max_edges):
+def eval_network_probes(annot_file, net_file, gs_file, 
+                        wt_attr, max_dist, max_edges, reverse_order):
     annot_df = du.load_annotation(annot_file)
-    gs_net = du.map_probes(du.load_gsnetwork(gs_file), annot_df)
-    print(gs_net.shape)
-    rv_net = select_edges(du.load_reveng_network(net_file),
+    gs_net = du.load_gsnetwork(gs_file)
+    gs_net = gs_net.loc[:, ['TF', 'TARGET']]
+    print("Network size (Before Probe Mapping)", gs_net.shape)
+    gs_net = du.map_probes(gs_net, annot_df)
+    print("Network size (After Probe Mapping)", gs_net.shape)
+    rv_net = select_edges(du.load_reveng_network(net_file, wt_attr_name=wt_attr),
                           wt_attr_name=wt_attr,
-                          max_edges=max_edges)
-    return eval_network(rv_net, gs_net, max_dist)
+                          max_edges=max_edges,
+                          reverse_order=reverse_order)
+    return eval_network(rv_net, gs_net, max_dist, wt_attr)
 
-def compare_eval_network_probes(annot_file, net_files, gs_file, wt_attr, max_dist, max_edges):
-    nhdat = [eval_network_probes(annot_file, fx, gs_file, wt_attr, max_dist, max_edges)
+def compare_eval_network_probes(annot_file, net_files, gs_file, wt_attr,
+                                max_dist, max_edges, reverse_order):
+    nhdat = [eval_network_probes(annot_file, fx, gs_file, wt_attr,
+                                 max_dist, max_edges, reverse_order)
              for fx in net_files]
+    clnames = ['NVRT', 'NEDG', 'GRGSV', 'GRGSE', 'GRCMV', 'GRCME', 'GRSPV', 'GRSPE'] + ['EDGN'+str(y) for y in range(max_dist+1)] + ['FP', 'TP', 'PREC', 'PRECPCT']
     gs_cmp_data = {str(net_files[x]) :
+                   [nhdat[x]['NVRT'], nhdat[x]['NEDG']] +
                    [nhdat[x]['GRGS'][0][0], nhdat[x]['GRGS'][0][1]] +
                    [nhdat[x]['GRCM'][0][0], nhdat[x]['GRCM'][0][1]] +
                    [nhdat[x]['GRSP'][0][0], nhdat[x]['GRSP'][0][1]] +
@@ -120,19 +135,23 @@ def compare_eval_network_probes(annot_file, net_files, gs_file, wt_attr, max_dis
                    [nhdat[x]['FPCTS'][0][0], nhdat[x]['FPCTS'][0][1],
                     nhdat[x]['FPCTS'][0][2], nhdat[x]['FPCTS'][0][3]]
                    for x in range(len(net_files))}
-    gs_cmp_data_df = pd.DataFrame(data=gs_cmp_data)
+    gs_cmp_data_df = pd.DataFrame(data=gs_cmp_data, index=clnames)
     print(gs_cmp_data_df.to_csv(sep='\t', index=True))
 
 
-def eval_network_ids(net_file, gs_file, wt_attr, max_dist, max_edges):
+def eval_network_ids(net_file, gs_file, wt_attr,
+                     max_dist, max_edges, reverse_order):
     gs_net = du.load_gsnetwork(gs_file)
     rv_net = select_edges(pd.read_csv(net_file, sep="\t"),
                           wt_attr_name=wt_attr,
-                          max_edges=max_edges)
-    return eval_network(rv_net, gs_net, max_dist, 'TF', 'TARGET')
+                          max_edges=max_edges,
+                          reverse_order=reverse_order)
+    return eval_network(rv_net, gs_net, max_dist, wt_attr, 'TF', 'TARGET')
 
-def compare_eval_network_ids(net_files, gs_file, wt_attr, max_dist, max_edges):
-    nhdat = [eval_network_ids(fx, gs_file, wt_attr, max_dist, max_edges)
+def compare_eval_network_ids(net_files, gs_file, wt_attr,
+                             max_dist, max_edges, reverse_order):
+    nhdat = [eval_network_ids(fx, gs_file, wt_attr,
+                              max_dist, max_edges, reverse_order)
              for fx in net_files]
     gs_cmp_data = {str(net_files[x]) :
                    [nhdat[x]['GRGS'][0][0], nhdat[x]['GRGS'][0][1]] +
@@ -144,6 +163,34 @@ def compare_eval_network_ids(net_files, gs_file, wt_attr, max_dist, max_edges):
                    for x in range(len(net_files))}
     gs_cmp_data_df = pd.DataFrame(data=gs_cmp_data)
     print(gs_cmp_data_df.to_csv(sep='\t', index=True))
+
+
+def compare_eval_network_probe_ranges(annot_file, net_files, gs_file,
+                                      wt_attr, max_dist,
+                                      eranges, step, reverse_order):
+    lstart = int(eranges.split(",")[0])
+    rend = int(eranges.split(",")[1])
+    edge_range = list(range(lstart, rend+1, step))
+    print(edge_range)
+    nhdat = [eval_network_probes(annot_file, fx, gs_file, 
+                                 wt_attr, max_dist, nedges, reverse_order)
+             for nedges in edge_range for fx in net_files]
+    print(len(nhdat))
+    clnames = ['GRGSV', 'GRGSE', 'GRCMV', 'GRCME', 'GRSPV', 'GRSPE'] + ['EDGN'+str(y) for y in range(max_dist+1)] + ['FP', 'TP', 'PREC', 'PRECPCT']
+    nranges = len(edge_range)
+    nfiles = len(net_files)
+    nentries = nranges * nfiles
+    gs_cmp_data = {str(net_files[0]) + "-" + str(edge_range[x % nranges]):
+                   [nhdat[x]['GRGS'][0][0], nhdat[x]['GRGS'][0][1]] +
+                   [nhdat[x]['GRCM'][0][0], nhdat[x]['GRCM'][0][1]] +
+                   [nhdat[x]['GRSP'][0][0], nhdat[x]['GRSP'][0][1]] +
+                   [nhdat[x]['EDGN'][y] for y in range(max_dist+1)] +
+                   [nhdat[x]['FPCTS'][0][0], nhdat[x]['FPCTS'][0][1],
+                    nhdat[x]['FPCTS'][0][2], nhdat[x]['FPCTS'][0][3]]
+                   for x in range(nentries)}
+    gs_cmp_data_df = pd.DataFrame(data=gs_cmp_data, index=clnames)
+    print(gs_cmp_data_df.to_csv(sep='\t', index=True))
+
 
 if __name__ == "__main__":
     PROG_DESC = """
@@ -166,13 +213,41 @@ if __name__ == "__main__":
     PARSER.add_argument("-t", "--wt_attr", type=str, default='wt',
                         help="name of weight attribute")
     PARSER.add_argument("-x", "--max_edges", type=int,
-                        help="""comma seperated names of the network;
-                                should have as many names as the number of networks""")
+                        help="""Maximum number of edges""")
+    PARSER.add_argument("-n", "--range_edges", type=str,
+                        help="""Range of edges""")
+    PARSER.add_argument("-s", "--range_steps", type=int, default=100000,
+                        help="""Steps of edges""")
+    PARSER.add_argument("-r", "--reverse_order", action='store_true',
+                        help="""Order the edges ascending order""")
     ARGS = PARSER.parse_args()
-    if ARGS.annotation_file == "NULL":
-        compare_eval_network_ids(ARGS.reveng_network_files,
-                                 ARGS.gs_network_file, ARGS.wt_attr,
-                                 ARGS.dist, ARGS.max_edges)
+    if ARGS.range_edges is None:
+        if ARGS.annotation_file == "NULL":
+            compare_eval_network_ids(ARGS.reveng_network_files,
+                                     ARGS.gs_network_file, ARGS.wt_attr,
+                                     ARGS.dist, ARGS.max_edges,
+                                     ARGS.reverse_order)
+        else:
+            compare_eval_network_probes(ARGS.annotation_file, 
+                                        ARGS.reveng_network_files,
+                                        ARGS.gs_network_file,
+                                        ARGS.wt_attr,
+                                        ARGS.dist,
+                                        ARGS.max_edges,
+                                        ARGS.reverse_order)
     else:
-        compare_eval_network_probes(ARGS.annotation_file, ARGS.reveng_network_files,
-                            ARGS.gs_network_file, ARGS.wt_attr, ARGS.dist, ARGS.max_edges)
+        if ARGS.annotation_file == "NULL":
+            #compare_eval_network_ids(ARGS.reveng_network_files,
+            #                         ARGS.gs_network_file, ARGS.wt_attr,
+            #                         ARGS.dist, ARGS.max_edges)
+            print("Option Not supported Yet!")
+        else:
+            compare_eval_network_probe_ranges(ARGS.annotation_file, 
+                                              ARGS.reveng_network_files,
+                                              ARGS.gs_network_file,
+                                              ARGS.wt_attr,
+                                              ARGS.dist,
+                                              ARGS.range_edges,
+                                              ARGS.range_steps,
+                                              ARGS.reverse_order)
+
--- src/eval/topgo_predefined.R
+++ src/eval/topgo_predefined.R
@@ -6,10 +6,18 @@ library("stringr")
 
 run.top.GO <- function(iqr.vector, subset.vec, go.desc,
                        go.ont = "BP"){
-    geneList = factor(as.integer(subset.vec %in% names(iqr.vector)))
+    boolSelect = names(iqr.vector) %in% subset.vec
+    iqrtSelect = iqr.vector >= 0.65
+    biqrSelect = boolSelect & iqrtSelect
+    print(sum(boolSelect))
+    print(sum(iqrtSelect))
+    print(quantile(iqr.vector[boolSelect]))
+    print(sum(biqrSelect))
+    geneList = factor(as.integer(boolSelect))
+    geneList = factor(as.integer(biqrSelect))
     names(geneList) = names(iqr.vector)
     new("topGOdata", description = go.desc, ontology = go.ont,
-        allGenes = iqr.vector, 
+        allGenes = geneList, 
         nodeSize = 50, annot = annFUN.db, affyLib = "ath1121501.db")
 }
 
@@ -20,21 +28,35 @@ read.iqr.table = function(iqr.file) {
    iqrv
 }
 
-fisher.top.GO = function(iqr.vector, subset.vec, go.desc,
-                         go.ont = "BP"){
-    tgo.data = run.top.GO(iqrv, subgv, go.desc)
+
+
+run.fisher <- function(tgo.data){
+    runTest(tgo.data, algorithm = "classic", statistic = "fisher")
+}
+
+run.ks <- function(tgo.data) {
+    runTest(tgo.data, algorithm = "classic", statistic = "ks")
+}
+
+run.elim.ks <- function(tgo.data) {
+    runTest(tgo.data, algorithm = "elim", statistic = "ks")
+}
+
+fisher.top.GO = function(iqr.vec, subset.vec, go.desc,
+                         go.ont = "BP", ntop=50){
+    tgo.data = run.top.GO(iqr.vec, subset.vec, go.desc)
     fisher.results <- run.fisher(tgo.data)
     ks.results <- run.ks(tgo.data)
     elim.ks.results <- run.elim.ks(tgo.data)
     GenTable(tgo.data, classicFisher = fisher.results,
              classicKS = ks.results, elimKS = elim.ks.results,
-             orderBy = "elimKS", ranksOf="classicFisher", topNodes = ntop)
+             orderBy = "elimKS", ranksOf="classicFisher", topNodes = 20)
 
 }
 
 fisher.diff = function(iqrfile, subfile, outfile, godesc) {
     iqrv = read.iqr.table(iqrfile)
-    subgv = read.table(iqr.file)$V1
+    subgv = read.table(subfile)$V1
     gx.df = fisher.top.GO(iqrv, subgv, godesc)
     write.table(file=outfile, gx.df, sep="\t", quote=F, row.names = F)
 }
@@ -43,11 +65,11 @@ args = commandArgs(trailingOnly=TRUE)
 if(length(args) == 3){
     fisher.diff(iqrfile=args[1], subfile=args[2],
                 outfile=args[3], godesc="Predefined")
-} 
-else if(length(args) == 4){
+} else {
+ if(length(args) == 4){
     fisher.diff(iqrfile=args[1], subfile=args[2],
                 outfile=args[3], godesc=args[4])
-} 
-else {
+ } else {
    print("Usage: Rscript togo_enrichment.R <INFILE1> <SUBFILE> <OUTFILE1> ")
+ }
 }
