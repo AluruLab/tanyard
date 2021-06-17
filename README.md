f0dde739ceee1c3ad9c254a298206628322721bd
Sriram P C <sriram.pc@gmail.com>
2021-06-17 15:21:21 -0400

    added README

---
+++ README.md
@@ -0,0 +1,27 @@
+Workflow Scripts for Processing large-scale Microarray datasets.
+=======================================
+
+Scripts for quality control, MAS5 normalization, gene filtering and annotation are present in the
+src/qc, src/mas5, src/filter and src/annotation directories. 
+A workflow for constructing a gene expression matrix is as follows:
+
+1. Download the datsaets from GEO/ArrayExpress(AE) and organize them as follows:
+
+  `<EXP_DIR>` is the root directory, in which all datasets are placed.
+
+  Files are organized in the folders as below:
+
+    <EXP_DIR>/CEL/<ACCESS_NAME_DIR>/<CEL_FILES>
+
+  where `<ACCESS_NAME_DIR>` is the accession id, under which the files were
+  submitted in ArrayExpress (AE) or GEO.
+2. Run Quality control using the scripts in src/qc directory.
+3. Run MAS5 normalization using the scripts in src/mas5 directory.
+4. Run IQR Gene filter and quantile normalization using scripts  in src/filter directory.
+
+Please see the README files in respective directories on how to run these scripts. 
+To prepare annotation from probe elements, helper scripts are available in the src/annotation directory.
+
+
+data/ directory contains the annotation, reference networks, classification tables of
+A. thaliana datasets used in our paper
--- data/ref/merge_refset.py
+++ data/ref/merge_refset.py
@@ -11,7 +11,11 @@ INPUT_REFSET = {
     "ptregmerged"  : "PlantRegMap-Merged.tsv",
     "ptregcslit"   : "PlantRegMap-Chip-Seq-Lit.tsv",
     "ptregmotfbs"  : "PlantRegMap-Motif-TFBS.tsv",
-    "n2target"     : "n2target.tsv" 
+    "n2target"     : "n2target.tsv", 
+    "n2targetgrn"  : "n2targetgrn-full.tsv",
+    "n2targetgrn3" : "n2targetgrn-3P5.tsv",
+    "n2targetgrn9" : "n2targetgrn-9P5.tsv",
+    "biogridy1h"   : "BioGRID-Y1H.tsv"
 }
 
 INPUT_REFSET_COLS = {
@@ -25,7 +29,11 @@ INPUT_REFSET_COLS = {
     "ptregmerged"  : (0, 1),
     "ptregcslit"   : (0, 2),
     "ptregmotfbs"  : (0, 2),
-    "n2target"     : (0, 1)
+    "n2target"     : (0, 1),
+    "n2targetgrn"  : (0, 1),
+    "n2targetgrn3" : (0, 1),
+    "n2targetgrn9" : (0, 1),
+    "biogridy1h"   : (0, 1)
 }
 
 def merge_refset(edge_set, ref_file, tf_col=0, tgt_col=1):
@@ -42,10 +50,12 @@ def merge_refset(edge_set, ref_file, tf_col=0, tgt_col=1):
                 uvx = (elts[tgt_col], elts[tf_col])
             if uvx not in edge_set:
                 edge_set.add(uvx)
-                print("\t".join([elts[tf_col], elts[tgt_col]]))
+                #print("\t".join([elts[tf_col], elts[tgt_col]]))
+                print("-".join([uvx[0], uvx[1]]))
     return edge_set
 
 def main(ref_nets):
+    #print(ref_nets)
     print("\t".join(["TF", "TARGET"]))
     if "all" in ref_nets:
         ref_nets = list(INPUT_REFSET.keys())
